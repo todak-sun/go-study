7c33766bf595de1f577dacb7239c01a10811619d
todak-sun <tjsdydwn@gmail.com>
2021-06-18 22:52:46 +0900

    go routine

--- url-check.go
+++ url-check.go
@@ -6,11 +6,16 @@ import (
 	"net/http"
 )
 
+type requestResult struct {
+	url    string
+	status string
+}
+
 var errRequestFailed = errors.New("Request Fail")
 
 func main() {
-
-	var results = make(map[string]string)
+	results := make(map[string]string)
+	c := make(chan requestResult)
 
 	urls := []string{
 		"https://www.airbnb.com/",
@@ -24,26 +29,27 @@ func main() {
 		"https://academy.nomadcoders.co/",
 	}
 
-	results["gello"] = "Hello"
-
 	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
+		go hitURL(url, c)
 	}
 
-	fmt.Println(results)
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking:", url)
+func hitURL(url string, c chan<- requestResult) {
+	// send only channel
 	resp, err := http.Get(url)
+	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
-		return errRequestFailed
+		status = "FAILED"
 	}
-
-	return nil
+	c <- requestResult{url: url, status: status}
 }
